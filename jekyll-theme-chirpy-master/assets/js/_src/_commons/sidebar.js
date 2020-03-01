76ec3a79a97cda7d7a442906eda6c8df3865c788
Brian Pho <brianpho7@gmail.com>
2020-02-29 18:54:03 -0700

    Organize files and complete sidebar

--- jekyll-theme-chirpy-master/assets/js/_src/_commons/sidebar.js
+++ jekyll-theme-chirpy-master/assets/js/_src/_commons/sidebar.js
@@ -5,52 +5,21 @@
  * © 2018-2019 Cotes Chung
  * MIT License
  */
-$(function(){
+$(function() {
 
-  var isExpanded = false;
+  let isExpanded = false;
 
   $("#sidebar-trigger").click(function() {
-    if (isExpanded == false) {
+    if (!isExpanded) {
       $("#sidebar").addClass("sidebar-expand");
-      openModal();
       isExpanded = true;
     }
   });
 
   $("#mask").click(function() {
-    $("#sidebar").removeClass("sidebar-expand");
-    closeModal();
-    isExpanded = false;
-  });
-
-  /**
-  * ModalHelper helpers resolve the modal scrolling issue on mobile devices
-  * https://github.com/twbs/bootstrap/issues/15852
-  * requires document.scrollingElement polyfill https://github.com/yangg/scrolling-element
-  */
-  var ModalHelper = (function(bodyCls) {
-    var scrollTop;
-    return {
-      afterOpen: function() {
-        scrollTop = document.scrollingElement.scrollTop;
-        document.body.classList.add(bodyCls);
-        document.body.style.top = -scrollTop + 'px';
-      },
-      beforeClose: function() {
-        document.body.classList.remove(bodyCls);
-        // scrollTop lost after set position:fixed, restore it back.
-        document.scrollingElement.scrollTop = scrollTop;
-        document.body.style.top = '';
+    if (isExpanded) {
+      $("#sidebar").removeClass("sidebar-expand");
+      isExpanded = false;
       }
-    };
-  })('no-scroll');
-
-  function openModal() {
-    ModalHelper.afterOpen();
-  }
-
-  function closeModal() {
-    ModalHelper.beforeClose();
-  }
-
+  });
 });
