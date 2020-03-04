2ad0d9ec5cd2c94259f698be4f4ce0bc27ed470f
Brian Pho <brianpho7@gmail.com>
2020-03-04 00:40:14 -0700

    Almost complete site redesign

--- jekyll-theme-chirpy-master/assets/js/_src/sidebar.js
+++ jekyll-theme-chirpy-master/assets/js/_src/sidebar.js
@@ -1,22 +1,18 @@
 /**
  * Expand or close the sidebar in mobile screens.
- * v2.0
- * https://github.com/cotes2020/jekyll-theme-chirpy
- * © 2018-2019 Cotes Chung
- * MIT License
  */
 $(function() {
 
   let isExpanded = false;
 
-  $("#sidebar-trigger").click(function() {
+  $("#sidebar-trigger").click(() => {
     if (!isExpanded) {
       $("#sidebar").addClass("sidebar-expand");
       isExpanded = true;
     }
   });
 
-  $("#mask").click(function() {
+  $("#mask").click(() => {
     if (isExpanded) {
       $("#sidebar").removeClass("sidebar-expand");
       isExpanded = false;
