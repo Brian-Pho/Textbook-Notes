0a80fc7259f701cc4ff4ac8edbbde74e8d16728c
Brian Pho <brianpho7@gmail.com>
2020-05-23 15:51:31 -0600

    Complete sidebar buttons and add padding

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,9 +3,11 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import Nav from "react-bootstrap/Nav"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faMoon } from "@fortawesome/free-solid-svg-icons"
+import { faMoon, faAt } from "@fortawesome/free-solid-svg-icons"
 import "./sidebar.scss"
 import Col from "react-bootstrap/Col"
+
+const SIDEBAR_ICON_SIZE = "lg"
 
 export interface Menu {
   label: string
@@ -44,34 +46,39 @@ const Sidebar = (props: SidebarProps): JSX.Element => {
       xs={12}
       className="sidebar d-flex flex-column justify-content-end position-sticky p-4"
     >
-      <div className="header">
-        <Link className="site-title" to="/">
-          {title}
-        </Link>
-        <div className="site-desc mb-4">{description}</div>
-      </div>
-      <div className="menu">
-        <Nav
-          variant="pills"
-          defaultActiveKey={props.activePage}
-          className="flex-column"
-        >
-          {menu.map((item: Menu, index: number) => {
-            return (
-              <Nav.Link href={item.path} key={index} className="mt-2 mb-2">
-                {item.label}
-              </Nav.Link>
-            )
-          })}
-        </Nav>
-      </div>
-      <div className="buttons d-flex justify-content-center mt-4 mb-4">
-        <FontAwesomeIcon icon={faMoon} />
-        <FontAwesomeIcon icon={faGithub} />
-
-        {/*<a href={`https://github.com/${author.github}`}>*/}
-        {/*  <FontAwesomeIcon icon={faGithub} />*/}
-        {/*</a>*/}
+      <div className="p-4">
+        <div className="header">
+          <Link className="site-title" to="/">
+            {title}
+          </Link>
+          <div className="site-desc mb-4">{description}</div>
+        </div>
+        <div className="menu">
+          <Nav
+            variant="pills"
+            defaultActiveKey={props.activePage}
+            className="flex-column"
+          >
+            {menu.map((item: Menu, index: number) => {
+              return (
+                <Nav.Link href={item.path} key={index} className="mt-2 mb-2">
+                  {item.label}
+                </Nav.Link>
+              )
+            })}
+          </Nav>
+        </div>
+        <div className="buttons d-flex justify-content-around mt-4 mb-4">
+          <a href="/">
+            <FontAwesomeIcon icon={faMoon} size={SIDEBAR_ICON_SIZE} />
+          </a>
+          <a href={`https://github.com/${author.github}`}>
+            <FontAwesomeIcon icon={faGithub} size={SIDEBAR_ICON_SIZE} />
+          </a>
+          <a href="mailto:brainic777@gmail.com">
+            <FontAwesomeIcon icon={faAt} size={SIDEBAR_ICON_SIZE} />
+          </a>
+        </div>
       </div>
     </Col>
   )
