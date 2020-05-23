db32a300d61de0b9e8fdce9fe7a132dc9dd52bd6
Brian Pho <brianpho7@gmail.com>
2020-05-22 19:32:43 -0600

    Add note page temporary code

--- src/pages/notes.tsx
+++ src/pages/notes.tsx
@@ -1,6 +1,11 @@
 import React from "react"
 import Sidebar, { Menu } from "../components/sidebar/sidebar"
 import { graphql, useStaticQuery } from "gatsby"
+import Nav from "react-bootstrap/Nav"
+import Layout from "../components/layout/layout"
+import Col from "react-bootstrap/Col"
+
+const NOTE_CATEGORIES = ["Books", "Textbooks", "Papers", "Courses", "Other"]
 
 const Notes = (): JSX.Element => {
   const data = useStaticQuery(graphql`
@@ -21,10 +26,26 @@ const Notes = (): JSX.Element => {
   }).path
 
   return (
-    <div>
-      <Sidebar activePage={notesPath} />
-      <h1>Notes</h1>
-    </div>
+    <Layout activePath={notesPath}>
+      <Col xl={6} md={9} xs={12} className="p-4">
+        <Nav
+          justify
+          variant="pills"
+          defaultActiveKey={NOTE_CATEGORIES[0]}
+          className="p-4"
+        >
+          {NOTE_CATEGORIES.map((category, index) => {
+            return (
+              <Nav.Item key={index}>
+                <Nav.Link href="/notes" eventKey={category}>
+                  {category}
+                </Nav.Link>
+              </Nav.Item>
+            )
+          })}
+        </Nav>
+      </Col>
+    </Layout>
   )
 }
 
