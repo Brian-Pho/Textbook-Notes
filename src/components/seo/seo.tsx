a41a5f7340db23362e72d3bc96f8fd414846df86
Brian Pho <brianpho7@gmail.com>
2020-06-12 16:40:42 -0600

    Optimize imports

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useLocation } from "@reach/router"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 import { SiteMetadataType } from "../../utils/graphqlTypes"
 
 interface SEOProps {
